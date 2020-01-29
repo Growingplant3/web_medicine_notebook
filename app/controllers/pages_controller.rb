bf627694ee01b6eeb118bd6312963a9bd3e4ba35
takashi.isobe <issobero153@gmail.com>
2020-01-30 03:05:18 +0900

    20200130 isobe update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   # Redirect to login page if not login
   before_action :authenticate_user! , only: [:devise_test]
-　# devise_page create
+
   def devise_test
   end
 
