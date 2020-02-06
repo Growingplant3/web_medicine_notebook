a08d2d15be3bfd7a0fe040a652f26cff08200e9e
下田　佳紀 <y.s@experience.local>
2020-02-07 00:25:16 +0900

    20200207a

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,5 @@
 class Hospital < ApplicationRecord
     has_many :Telphones
+    enum holiday:{sunday:0,monday:1,tuesday:2,wednesday:3,thursday:4,friday:5,saturday:6}
+    enum emergency:{true:ok,false:ng}
 end
