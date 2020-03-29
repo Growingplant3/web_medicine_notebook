ee89b213847c9eac72f74734b593a3a194a8b52d
EC2 Default User <ec2-user@ip-172-31-36-46.us-east-2.compute.internal>
2020-03-29 12:05:39 +0000

    20200329 update

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   resources :vacations
   resources :hospitals
+  resources :users
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'hospitals/test'
   get 'hospitals/:id/edit' => 'hospitals#edit'
   get 'pages/index'
   get 'pages/devise_test'
-  get 'users', to: 'users#index'
-  get 'pages/allergy'
-  get 'pages/sick'
-  get 'pages/operation'
+  get 'hospitals/prescription'
   root "pages#index"
 end
